70da82a74166cc6f9a0b2889b67517bd012bb77d
Raven Lung <wlwang41@gmail.com>
2014-06-12 14:03:59 +0800

    init repo

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import cb
+
+
+entry_points = {
+    "console_scripts": [
+        "cb = cb.cli:main",
+    ]
+}
+
+requires = [
+    "Markdown",
+    "Pygments",
+    "Jinja2",
+    "PyYAML",
+    "docopt",
+]
+
+setup(
+    name="cb",
+    version=cb.__version__,
+    url="https://github.com/wlwang41/cb",
+    author="Crow(wlwang41)",
+    author_email="wlwang41@gmail.com",
+    description="Cb is a blog building tool based on tankywoo/simiki.",
+    license="MIT License",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requires,
+    entry_points=entry_points,
+)
