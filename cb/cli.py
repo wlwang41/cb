01568119cce171656e67ba84ec208758a8186286
Raven Lung <crowwang@sohu-inc.com>
2014-06-13 14:27:12 +0800

    dev

--- cb/cli.py
+++ cb/cli.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""This is the cli command.
 """
+Cb CLI
+
+Usage:
+  cb init
+  cb new -t <title> -c <category> [-f <file>]
+  cb generate [--delete]
+  cb server
+  cb -h | --help
+  cb -V | --version
+
+Options:
+  -h, --help             Help information.
+  -V, --version          Show version.
+  -c <category>          Specify the category.
+  -t <title>             Specify the new post title.
+  -f <file>              Specify the new post filename.
+  --delete               Delete the contents of output directory before generate.
+
+"""
+
 
 def main():
     pass
---
+++ cb/log.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import logging
+from logging import (
+    getLogger, Formatter, StreamHandler
+)
+
+from cb import utils
+
+
+class ANSIFormatter(Formatter):
+    """Use ANSI escape sequences to colored log"""
+
+    def format(self, record):
+        lvl2color = {
+            "DEBUG": "blue",
+            "INFO": "green",
+            "WARNING": "yellow",
+            "ERROR": "red",
+            "CRITICAL": "bgred"
+        }
+
+        msg = record.getMessage()
+        rln = record.levelname
+        if rln in lvl2color:
+            return "[{}]: {}".format(utils.color_msg(lvl2color[rln], rln), msg.encode('utf-8'))
+        else:
+            return msg
+
+
+def logging_init(level=None, logger=getLogger(), handler=StreamHandler(), use_color=True):
+    if use_color:
+        fmt = ANSIFormatter()
+    else:
+        fmt = Formatter()
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+
+    if level:
+        logger.setLevel(level)
