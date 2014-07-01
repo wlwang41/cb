e96008639c83034484290f77ddb4e75575cc0217
Raven Lung <wlwang41@gmail.com>
2014-07-01 23:47:49 +0800

    add some code structure

--- cb/cli.py
+++ cb/cli.py
@@ -5,9 +5,9 @@
 Cb CLI
 
 Usage:
-  cb init
+  cb init [-p <path>]
   cb new -t <title> -c <category> [-f <file>]
-  cb generate [--delete]
+  cb build [--delete]
   cb server
   cb -h | --help
   cb -V | --version
@@ -18,13 +18,46 @@ Options:
   -c <category>          Specify the category.
   -t <title>             Specify the new post title.
   -f <file>              Specify the new post filename.
+  -p <path>              Destination path.
   --delete               Delete the contents of output directory before generate.
 
 """
 
+import os
+import logging
+
+from docopt import docopt
+
+from cb import __version__
+from cb.log import logging_init
+
+logger = logging.getLogger(__name__)
+
 
 def main():
-    pass
+    logging_init(logging.DEBUG)
+    # Get input info from docopt.
+    args = docopt(__doc__, version='cb {}'.format(__version__))
+
+    # Init the blog codes.
+    if args['init']:
+        # The generated blog code's path.
+        blog_root_path = args['-p'] if args['-p'] else os.getcwd()
+
+    # Start a web server to debug.
+    elif args['server']:
+        pass
+
+    # Build the source.
+    elif args['build']:
+        pass
+
+    # Post a new md.
+    elif args['new']:
+        pass
+
+    logger.info('Done.')
+
 
 if __name__ == '__main__':
     main()
